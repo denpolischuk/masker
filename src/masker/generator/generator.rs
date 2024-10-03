d97d6413fbbd845bb67234fc1eab429274424add
Denys Polishchuk <den.polischuk19@gmail.com>
2024-10-03 18:53:03 +0200

    fix: string value templ

--- src/masker/generator/generator.rs
+++ src/masker/generator/generator.rs
@@ -20,7 +20,7 @@ impl Display for GeneratedValue {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match &self {
             GeneratedValue::Number(n) => write!(f, "{n}"),
-            GeneratedValue::String(s) => write!(f, "'{s}'"),
+            GeneratedValue::String(s) => write!(f, "\"{s}\""),
         }
     }
 }
