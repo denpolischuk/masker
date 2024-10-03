a3e0b1ad5c0d60790f5f3769ada6d5d5a645b634
Denys Polishchuk <den.polischuk19@gmail.com>
2024-10-03 18:56:56 +0200

    chore: comment'

--- src/masker/generator/simple_generator.rs
+++ src/masker/generator/simple_generator.rs
@@ -7,7 +7,7 @@ pub struct SimpleGenerator {
 // This is a wrapper struct that implements Generator trait for simple generator functions, like
 // first name, last name and so on, that don't require any configuration from the user.
 // These functions are simple thus are just implemented as public functions and can be re-used. The
-// wrappers is used for type consistency in global masker object.
+// wrapper is used for the type consistency in a global masker object.
 impl SimpleGenerator {
     pub fn new(generator_fn: GeneratorFunction) -> Self {
         Self {
